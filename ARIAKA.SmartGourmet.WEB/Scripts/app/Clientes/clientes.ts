40fc117cc94e4851fb12ed43639bc47ee8e8b28f
Francisco Chacón <francisco.chacont@gmail.com>
2019-02-01 17:22:06 -0300

    SM-17-4 Añadida vista, con su funcionalidades respectivas en script.

--- ARIAKA.SmartGourmet.WEB/Scripts/app/Clientes/clientes.ts
+++ ARIAKA.SmartGourmet.WEB/Scripts/app/Clientes/clientes.ts
@@ -6,6 +6,9 @@ namespace Clientes {
     'use strict'
     export class ClientesIndexViewModel {
         public clientes: KnockoutObservableArray<any> = ko.observableArray<any>();
+        public enable: KnockoutObservable<boolean> = ko.observable(true);
+        public idRow: KnockoutObservable<number> = ko.observable(0);
+        public idRowIndex: KnockoutObservable<number> = ko.observable(-1);
 
         buttonOptions: DevExpress.ui.dxButtonOptions = {
             text: "Soy un botón",
@@ -92,24 +95,151 @@ namespace Clientes {
                 this.limpiarForm();
                 grid.refresh();
                 grid.repaint();
-            }).fail((data: any) => {
-                DevExpress.ui.notify(data.responseJSON, "error", 3000);
-            });
+                }).fail((data: any) => {
+                    DevExpress.ui.notify(data.responseJSON, "error", 3000);
+                });
         }
 
         deleteCliente(id: number): void {
-            let url = 'api/clientes' + id;
+            let url = 'api/clientes/' + id;
             $.ajax({
                 type: 'DELETE',
                 url: url
             }).done((data: any) => {
                 $('#form-clientes').dxForm('instance').resetValues();
                 this.limpiarForm();
-            });
+                }).fail((data: any) => {
+                    DevExpress.ui.notify(data.responseJSON, "error", 3000);
+                });
+        };
+
+        constructor() {
+            this.getClientes();
         }
 
-        // Constructor???
+        formOptions: any = {
+            formData: this.clientes,
+            labelLocation: "top",
+            items: [{
+                itemType: "group",
+                colCount: 3,
+                items: [{
+                    dataField: "Nombre",
+                    editorType: "dxTextBox",
+                    editorOptions: {
+                        label: "Nombre",
+                        showClearButton: true
+                    }
+                }, {
+                    dataField: "Apellido",
+                    editorType: "dxTextBox",
+                    editorOptions: {
+                        label: "Apellido",
+                        showClearButton: true
+                    }
+                }, {
+                    dataField: "Direccion",
+                    editorType: "dxTextBox",
+                    editorOptions: {
+                        label: "Dirección",
+                        showClearButton: true
+                    }
+                }, {
+                    dataField: "Telefono",
+                    editorType: "dxNumberBox",
+                    editorOptions: {
+                        label: "Teléfono",
+                        showClearButton: true
+                    }
+                }]
+            }]
+        };
 
+        buttonOptionsClear: any = {
+            text: "Limpiar",
+            icon: "refresh",
+            type: "default",
+            disabled: this.enable,
+            onClick: () => {
+                $('#form-clientes').dxForm('instance').resetValues();
+                this.limpiarForm();
+                this.idRow(0);
+            }
+        }
 
+        buttonOptionsDelete: any = {
+            text: "Borrar",
+            icon: "remove",
+            type: 'danger',
+            disabled: this.enable,
+            onClick: () => {
+                let grid = $('#grid-clientes').dxDataGrid('instance');
+                let index = this.idRow();
+                grid.deleteRow(this.idRowIndex());
+                grid.repaint();
+                this.deleteCliente(index);
+            }
+        }
+
+        buttonOptionsAdd: any = {
+            text: "Agregar",
+            icon: "plus",
+            type: 'success',
+            onClick: () => {
+                this.addClientes();
+            }
+        }
+
+        dataGridOptions: any = {
+            dataSource: this.clientes,
+            loadPanel: {
+                enabled: true,
+                text: 'Cargando datos...'
+            },
+            selection: {
+                mode: "single"
+            },
+            columns: [{ dataField: 'ID', visible: false }, 'Nombre', 'Apellido', 'Telefono', 'Direccion'],
+            editing: {
+                texts: {
+                    confirmDeleteMessage: '¿Esta seguro en eliminar registro?'
+                }
+            }, grouping: {
+                allowCollapsing: true
+            }, groupPanel: {
+                allowColumnDragging: true,
+                visible: true,
+                emptyPanelText: 'Arrastre algunas columnas para agrupar'
+            }, export: {
+                allowExportSelectedData: true,
+                enabled: true,
+                fileName: 'clientes'
+            }, columnChooser: {
+                allowSearch: true
+            },
+            showBorders: true
+            , rowAlternationEnabled: true
+            , showRowLines: true
+            , showColumnLines: false
+            , filterRow: {
+                visible: true,
+                showOperationChooser: false
+            }, onRowClick: (e) => {
+                this.enable(false);
+                let formData: any = $('#form-clientes').dxForm('option');
+                let clienteData: any = {
+                    ID: e.data.ID,
+                    Nombre: e.data.Nombre,
+                    Apellido: e.data.Apellido,
+                    Telefono: e.data.Telefono,
+                    Direccion: e.data.Direccion
+                }
+                this.idRow(clienteData.ID);
+                this.idRowIndex(e.rowIndex);
+                formData.formData = clienteData;
+                let form = $('#form-clientes').dxForm('instance');
+                form.repaint();
+            }
+        }
     }
 }
--- ARIAKA.SmartGourmet.WEB/Views/Clientes/Index.vbhtml
+++ ARIAKA.SmartGourmet.WEB/Views/Clientes/Index.vbhtml
@@ -2,8 +2,26 @@
     ViewData("Title") = "Index"
 End Code
 
-<h2>Cliente</h2>
-<div id="botonCrear" data-bind="dxButton: buttonOptions"></div>
+<div id="titulo" class="long-title"><h3>Creación Clientes</h3></div>
+
+<div id="cuerpo" style="margin-bottom:70px" class="dx-fieldset">
+    <div class="dx-field">
+        <div id="form-clientes" data-bind="dxForm: formOptions"></div> <!--??-->
+    </div>
+
+    <div class="dx-field">
+        <div id="botonesDetalle" class="btn-group" role="group">
+            <div id="botonCrear" data-bind="dxButton: buttonOptionsClear"></div>
+            <div id="botonCrear" data-bind="dxButton: buttonOptionsDelete"></div>
+            <div id="botonCrear" data-bind="dxButton: buttonOptionsAdd"></div>
+        </div>
+    </div>
+
+    <div class="dx-field">
+        <div id="grid-clientes" data-bind="dxDataGrid: dataGridOptions"></div>
+    </div>
+</div>
+
 
 @section scripts
     <script type="text/javascript" src="~/Scripts/app/Clientes/clientes.js"></script>
