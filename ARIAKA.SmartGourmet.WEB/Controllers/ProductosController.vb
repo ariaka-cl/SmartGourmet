20c0866e3d46895175da7ef32641e6ab8c2447ad
Francisco Chacón <francisco.chacont@gmail.com>
2019-02-11 21:22:45 -0300

    SM-20-1 Añadido los archivos principales, creadas funciones en API para rescatar datos, creado DTO, y vista.

---
+++ ARIAKA.SmartGourmet.WEB/Controllers/ProductosController.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    <RoutePrefix("productos")>
+    Public Class ProductosController
+        Inherits Controller
+
+        <Route("")>
+        Function Index() As ActionResult
+            Return View()
+        End Function
+    End Class
+End Namespace
